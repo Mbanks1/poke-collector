d911898ad0e595166d484e7982195648b235b0be
Mbanks1 <michael1banks@icloud.com>
2021-01-30 18:07:55 -0400

    poke model and list

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+TRAINERS = (
+    ('1', 'Trainer One'),
+    ('2', 'Trainer Two'),
+    ('3', 'Trainer Three'),
+)
+
+class Pokemon(models.Model):
+    name=models.CharField(max_length=50)
+    description = models.TextField(max_length=250)
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
-
+from .models import Pokemon
 # Create your views here.
 # Add the following import
-from django.http import HttpResponse
+
 
 # Define the home view
 def home(request):
-  return render(request, '<h1>Hello</h1>')
+  return render(request, 'home.html')
 def about(request):
    return render(request, 'about.html')
+   
+def pokemon_index(request):
+    return render(request, 'pokemon/index.html', {'pokemon': pokemon_index})
