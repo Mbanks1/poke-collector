60c5ecae1e770eaf2d53964986f619e1d84b7454
Mbanks1 <michael1banks@icloud.com>
2021-01-31 17:15:25 -0400

    pokemon details

--- main_app/views.py
+++ main_app/views.py
@@ -7,8 +7,14 @@ from .models import Pokemon
 # Define the home view
 def home(request):
   return render(request, 'home.html')
+
 def about(request):
    return render(request, 'about.html')
    
 def pokemon_index(request):
-    return render(request, 'pokemon/index.html', {'pokemon': pokemon_index})
+    pokemon = Pokemon.objects.all()
+    return render(request, 'pokemon/index.html', {'pokemon': pokemon})
+
+def pokemon_detail(request, pokemon_id):
+    pokemon = Pokemon.objects.get(id=pokemon_id)
+    return render(request, 'pokemon/detail.html', {'pokemon': pokemon})
